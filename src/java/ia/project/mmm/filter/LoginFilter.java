038752888a62006ca27608eea013cdfeed877061
Mohamed Kamal Kamaly <mohamedkomalo@hotmail.com>
2014-12-22 05:49:48 +0200

    initial implementation of LoginFilter, if the user does not have a session, he will be redirected to login page

--- src/java/ia/project/mmm/filter/LoginFilter.java
+++ src/java/ia/project/mmm/filter/LoginFilter.java
@@ -13,6 +13,8 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Redirects to Signin page if user has no session
@@ -20,13 +22,34 @@ import javax.servlet.ServletResponse;
  */
 public class LoginFilter implements Filter{
 
+    public String[] loginUrls = {"/login.html", "/login"};
+    
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest)request;
+        HttpServletResponse res = (HttpServletResponse)response;
         
+        String url = req.getServletPath();
+        
+        boolean hasSession = (req.getSession(false) != null);
+        
+        boolean goingToLogin = false;
+        for(String loginUrl : loginUrls){
+            goingToLogin |= url.equals(loginUrl);
+        }
+        
+        if(hasSession && goingToLogin ){
+            res.sendRedirect("index.jsp");
+        }
+        else if(!hasSession && !goingToLogin){
+            res.sendRedirect("login.html");
+        }
+        else
+            chain.doFilter(request, response);
     }
 
     @Override
